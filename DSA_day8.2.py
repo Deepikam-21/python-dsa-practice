d104b9585638b9437d2bca19696b152db54e9834
Deepika M <deepikamdeepika12@gmail.com>
2026-03-05 21:53:33 +0530

    Added DSA program to count even and odd numbers in array

---
+++ DSA_day8.2.py
@@ -0,0 +1,10 @@
+arr = [1, 2, 3, 4, 5, 6]
+even = 0
+odd = 0
+for i in arr:
+    if i % 2 == 0:
+        even += 1
+    else:
+        odd += 1
+print("Even numbers:", even)
+print("Odd numbers:", odd)
