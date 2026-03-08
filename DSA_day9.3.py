4031e6108eb5e15fb2ed3933188e92075d17cc69
Deepika M <deepikamdeepika12@gmail.com>
2026-03-08 22:01:28 +0530

    Added DSA program to check if element exists in array

---
+++ DSA_day9.3.py
@@ -0,0 +1,8 @@
+# Check if element exists in array
+
+arr = [3, 7, 1, 9, 4]
+key = int(input("Enter element to search: "))
+if key in arr:
+    print("Element found")
+else:
+    print("Element not found")
