0d0fa388aa711868d7f8abb0b0c9ef8e1c2c4779
Deepika M <deepikamdeepika12@gmail.com>
2026-03-08 22:00:47 +0530

    Added DSA program to find smallest and largest element in array

---
+++ DSA_day9.2.py
@@ -0,0 +1,7 @@
+# Find smallest and largest element in array
+
+arr = [5, 10, 2, 8, 15]
+smallest = min(arr)
+largest = max(arr)
+print("Smallest element:", smallest)
+print("Largest element:", largest)
